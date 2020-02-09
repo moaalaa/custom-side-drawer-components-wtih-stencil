c6a435b0d4b7e716b7dacb5b1cb235fe79bb0173
moaalaa <moaalaa16@gmail.com>
2020-02-09 14:01:28 +0200

    Added: Adding Side Links

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,15 +1,15 @@
 import { Component, h, Prop } from "@stencil/core";
 
 @Component({
-  tag: "mxcd-side-drawer",
-  styleUrl: "side-drawer.css",
-  shadow: true
+	tag: "mxcd-side-drawer",
+	styleUrl: "side-drawer.css",
+	shadow: true
 })
 export class SideDrawer {
-	
+
 	// Title Prop
 	// reflect: true => reflect changes to Component Attributes if changes happened.
-  	@Prop({reflect: true}) sideTitle: string;
+	@Prop({ reflect: true }) sideTitle: string;
 
 	render() {
 		return (
