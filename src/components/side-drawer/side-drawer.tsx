f9b684765013f96c2e106ffd9f737eb759d95411
moaalaa <moaalaa16@gmail.com>
2020-02-09 14:09:33 +0200

    Added: Adding Side Animations and open conditions

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -10,8 +10,26 @@ export class SideDrawer {
 	// Title Prop
 	// reflect: true => reflect changes to Component Attributes if changes happened.
 	@Prop({ reflect: true }) sideTitle: string;
+	// @Prop({ reflect: true }) open: boolean = false;
 
 	render() {
+		// let content = null;
+
+		// if (this.open) {
+		// 	content = (
+		// 		<aside>
+		// 			<header>
+		// 				<h1>{this.sideTitle}</h1>
+		// 			</header>
+		// 			<main>
+		// 				<slot></slot>
+		// 			</main>
+		// 		</aside>
+		// 	)
+		// }
+		
+		// return content;
+
 		return (
 			<aside>
 				<header>
